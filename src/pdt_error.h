450ac572a024d296c5017d831c6b48277f57bd34
Daniel Peace <danielpeace805@gmail.com>
2026-09-06 12:40:23 -0700

    [main] Added basic error codes

---
+++ src/pdt_error.h
@@ -0,0 +1,15 @@
+#ifndef PDT_ERROR_HEADER
+#define PDT_ERROR_HEADER
+
+#include "../../pdt-string/src/pdt_string.h"
+
+/**
+ * The current list of errors supported by PDT_Error.
+ */
+typedef enum
+{
+  OK = 0,
+  ERROR = 1
+} PDT_Error;
+
+#endif
